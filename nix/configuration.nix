d565576037baca25714260234be086b81c90d993
Maddy <maddy.lea@gmail.com>
2023-11-11 13:29:38 -0500

    add ntfs

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -16,7 +16,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.supportedFilesystems = [ "ntfs" ];
 
   ## NETWORK
 
